5203560cb04a094f2857accbd4786906ff3d1f64
Carlos Zamora <czamora5@ucol.mx>
2022-07-13 14:07:26 -0500

    Added styles

--- src/main.js
+++ src/main.js
@@ -1,72 +1,104 @@
-import Account from './account.js';
+import Account from "./account.js";
 
 function randomNumber(min, max, precision = 0) {
-  return Math.trunc((min + Math.random() * (max - min) * Math.pow(10, precision))) / Math.pow(10, precision);
+  return (
+    Math.trunc(min + Math.random() * (max - min) * Math.pow(10, precision)) /
+    Math.pow(10, precision)
+  );
 }
 
-const content = document.querySelector('.content');
+const content = document.querySelector(".content");
 const accounts = [];
 
-accounts.push(new Account("Geraldine", "Alanis", "geraldine", randomNumber(10, 990, 2)));
-accounts.push(new Account("Federico", "Rentería", "federico", randomNumber(10, 990, 2)));
-accounts.push(new Account("Alberto", "Altamirano", "alberto", randomNumber(10, 990, 2)));
-accounts.push(new Account("Fernanda", "Contreras", "fernanda", randomNumber(10, 990, 2)));
+accounts.push(
+  new Account("Geraldine", "Alanis", "geraldine", randomNumber(10, 990, 2))
+);
+accounts.push(
+  new Account("Federico", "Rentería", "federico", randomNumber(10, 990, 2))
+);
+accounts.push(
+  new Account("Alberto", "Altamirano", "alberto", randomNumber(10, 990, 2))
+);
+accounts.push(
+  new Account("Fernanda", "Contreras", "fernanda", randomNumber(10, 990, 2))
+);
+
+function createHeader() {
+  const header = document.createElement("header");
+  header.classList.add("navbar");
+  header.innerHTML = `
+    <div class="navbar-content">
+      <a href="https://devf.mx/" target="_blank">
+        <img height="32px" src="./assets/logo.png" alt="DEVF logo">
+      </a>
+      <div>Banco DEV.F</div>
+    </div>
+  `;
+
+  document.body.insertBefore(header, content);
+}
 
 function showWithdrawForm(account) {
-  content.innerHTML = '';
-  
-  const greeting = document.createElement('h1');
+  content.innerHTML = "";
+
+  const greeting = document.createElement("h1");
   greeting.innerHTML = `Ingresa la cantidad que deseas retirar de tu cuenta`;
   content.appendChild(greeting);
-  
-  const balance = document.createElement('h3');
+
+  const balance = document.createElement("h3");
   balance.innerText = `Tu saldo actual es ${account.getBalance()}`;
   content.appendChild(balance);
 
-  const formContainer = document.createElement('div');
-  const label = document.createElement('label');
-  label.setAttribute('for', 'quantityInput');
-  label.innerHTML = 'Cantidad a retirar: <br>';
+  const formContainer = document.createElement("div");
+  formContainer.classList.add("container");
+  const label = document.createElement("label");
+  label.setAttribute("for", "quantityInput");
+  label.innerHTML = "Cantidad a retirar: <br>";
   formContainer.appendChild(label);
 
-  const quantityInput = document.createElement('input');
-  label.setAttribute('type', 'number');
-  label.setAttribute('id', 'quantityInput');
+  const quantityInput = document.createElement("input");
+  quantityInput.setAttribute("type", "number");
+  quantityInput.setAttribute("id", "quantityInput");
   formContainer.appendChild(quantityInput);
 
-  const errorMsg = document.createElement('div');
-  errorMsg.style.color = 'red';
+  const errorMsg = document.createElement("div");
+  errorMsg.style.textAlign = "center";
+  errorMsg.style.color = "#ee3344";
   formContainer.appendChild(errorMsg);
 
   content.appendChild(formContainer);
 
-  const actions = document.createElement('div');
+  const actions = document.createElement("div");
 
-  const continueButton = document.createElement('button');
-  continueButton.innerText = 'Continuar';
+  const continueButton = document.createElement("button");
+  continueButton.innerText = "Continuar";
   actions.appendChild(continueButton);
 
-  continueButton.addEventListener('click', () => {
+  continueButton.addEventListener("click", () => {
     let quantity = Number(quantityInput.value);
     if (quantity <= 0) {
-      errorMsg.innerHTML = 'Favor de ingresar una cantidad válida';
+      errorMsg.innerHTML = "Favor de ingresar una cantidad válida";
       return;
     }
 
     let result = account.transaction(-quantity);
 
     if (result.completed) {
-      showAccountData(account, result.info + `, se han retirado \$${quantity} pesos del saldo de tu cuenta`);
+      showAccountData(
+        account,
+        result.info +
+          `, se han retirado \$${quantity} pesos del saldo de tu cuenta`
+      );
     } else {
       errorMsg.innerHTML = result.info;
     }
   });
 
-  const exitButton = document.createElement('button');
-  exitButton.innerText = 'Regresar';
+  const exitButton = document.createElement("button");
+  exitButton.innerText = "Regresar";
   actions.appendChild(exitButton);
 
-  exitButton.addEventListener('click', () => {
+  exitButton.addEventListener("click", () => {
     showAccountData(account);
   });
 
@@ -74,112 +106,119 @@ function showWithdrawForm(account) {
 }
 
 function showDepositForm(account) {
-  content.innerHTML = '';
-  
-  const greeting = document.createElement('h1');
+  content.innerHTML = "";
+
+  const greeting = document.createElement("h1");
   greeting.innerHTML = `Ingresa la cantidad que deseas depositar a tu cuenta`;
   content.appendChild(greeting);
-  
-  const balance = document.createElement('h3');
+
+  const balance = document.createElement("h3");
   balance.innerText = `Tu saldo actual es ${account.getBalance()}`;
   content.appendChild(balance);
 
-  const formContainer = document.createElement('div');
-  const label = document.createElement('label');
-  label.setAttribute('for', 'quantityInput');
-  label.innerHTML = 'Cantidad a depositar: <br>';
+  const formContainer = document.createElement("div");
+  formContainer.classList.add("container");
+  const label = document.createElement("label");
+  label.setAttribute("for", "quantityInput");
+  label.innerHTML = "Cantidad a depositar: <br>";
   formContainer.appendChild(label);
 
-  const quantityInput = document.createElement('input');
-  label.setAttribute('type', 'number');
-  label.setAttribute('id', 'quantityInput');
+  const quantityInput = document.createElement("input");
+  quantityInput.setAttribute("type", "number");
+  quantityInput.setAttribute("id", "quantityInput");
   formContainer.appendChild(quantityInput);
 
-  const errorMsg = document.createElement('div');
-  errorMsg.style.color = 'red';
+  const errorMsg = document.createElement("div");
+  errorMsg.style.textAlign = "center";
+  errorMsg.style.color = "#ee3344";
   formContainer.appendChild(errorMsg);
 
   content.appendChild(formContainer);
 
-  const actions = document.createElement('div');
+  const actions = document.createElement("div");
 
-  const continueButton = document.createElement('button');
-  continueButton.innerText = 'Continuar';
+  const continueButton = document.createElement("button");
+  continueButton.innerText = "Continuar";
   actions.appendChild(continueButton);
 
-  continueButton.addEventListener('click', () => {
+  continueButton.addEventListener("click", () => {
     let quantity = Number(quantityInput.value);
     if (quantity <= 0) {
-      errorMsg.innerHTML = 'Favor de ingresar una cantidad válida';
+      errorMsg.innerHTML = "Favor de ingresar una cantidad válida";
       return;
     }
 
     let result = account.transaction(quantity);
 
     if (result.completed) {
-      showAccountData(account, result.info + `, se han depositado \$${quantity} pesos al saldo de tu cuenta`);
+      showAccountData(
+        account,
+        result.info +
+          `, se han depositado \$${quantity} pesos al saldo de tu cuenta`
+      );
     } else {
       errorMsg.innerHTML = result.info;
     }
   });
 
-  const exitButton = document.createElement('button');
-  exitButton.innerText = 'Regresar';
+  const exitButton = document.createElement("button");
+  exitButton.innerText = "Regresar";
   actions.appendChild(exitButton);
 
-  exitButton.addEventListener('click', () => {
+  exitButton.addEventListener("click", () => {
     showAccountData(account);
   });
 
   content.appendChild(actions);
 }
 
-function showAccountData(account, lastTransactionMsg = '') {
-  content.innerHTML = '';
-  
-  const greeting = document.createElement('h1');
-  greeting.innerHTML = `Bienvenido ${account.getName()}`;
+function showAccountData(account, lastTransactionMsg = "") {
+  content.innerHTML = "";
+
+  const greeting = document.createElement("h1");
+  greeting.innerHTML = `Bienvenid@ ${account.getName()}`;
   content.appendChild(greeting);
 
-  const lastTransactionMessage = document.createElement('h3');
+  const lastTransactionMessage = document.createElement("h3");
   lastTransactionMessage.innerText = lastTransactionMsg;
-  lastTransactionMessage.style.color = 'green';
+  lastTransactionMessage.style.color = "#90EE90";
   content.appendChild(lastTransactionMessage);
 
-  const balanceContainer = document.createElement('div');
-  const balanceMsg = document.createElement('h2');
-  balanceMsg.innerHTML = 'El saldo en tu cuenta es';
+  const balanceContainer = document.createElement("div");
+  balanceContainer.classList.add("container");
+  const balanceMsg = document.createElement("h2");
+  balanceMsg.innerHTML = "El saldo en tu cuenta es";
   balanceContainer.appendChild(balanceMsg);
 
-  const balance = document.createElement('h2');
+  const balance = document.createElement("h2");
   balance.innerHTML = account.getBalance();
   balanceContainer.appendChild(balance);
 
   content.appendChild(balanceContainer);
 
-  const actions = document.createElement('div');
+  const actions = document.createElement("div");
 
-  const depositButton = document.createElement('button');
-  depositButton.innerText = 'Depositar';
+  const depositButton = document.createElement("button");
+  depositButton.innerText = "Depositar";
   actions.appendChild(depositButton);
 
-  depositButton.addEventListener('click', () => {
+  depositButton.addEventListener("click", () => {
     showDepositForm(account);
   });
 
-  const withdrawButton = document.createElement('button');
-  withdrawButton.innerText = 'Retirar';
+  const withdrawButton = document.createElement("button");
+  withdrawButton.innerText = "Retirar";
   actions.appendChild(withdrawButton);
 
-  withdrawButton.addEventListener('click', () => {
+  withdrawButton.addEventListener("click", () => {
     showWithdrawForm(account);
   });
 
-  const exitButton = document.createElement('button');
-  exitButton.innerText = 'Regresar';
+  const exitButton = document.createElement("button");
+  exitButton.innerText = "Regresar";
   actions.appendChild(exitButton);
 
-  exitButton.addEventListener('click', () => {
+  exitButton.addEventListener("click", () => {
     showAccounts();
   });
 
@@ -187,48 +226,49 @@ function showAccountData(account, lastTransactionMsg = '') {
 }
 
 function showValidationForm(account) {
-  content.innerHTML = '';
-  
-  const greeting = document.createElement('h1');
-  greeting.innerHTML = `Bienvenido ${account.getName()}, ingresa tu contraseña para continuar`;
+  content.innerHTML = "";
+
+  const greeting = document.createElement("h1");
+  greeting.innerHTML = `Bienvenid@ ${account.getName()}, ingresa tu contraseña para continuar`;
   content.appendChild(greeting);
 
-  const formContainer = document.createElement('div');
-  const label = document.createElement('label');
-  label.setAttribute('for', 'passwordInput');
-  label.innerHTML = 'Contraseña: <br>';
+  const formContainer = document.createElement("div");
+  formContainer.classList.add("container");
+  const label = document.createElement("label");
+  label.setAttribute("for", "passwordInput");
+  label.innerHTML = "Contraseña: <br>";
   formContainer.appendChild(label);
 
-  const passwordInput = document.createElement('input');
-  label.setAttribute('id', 'passwordInput');
+  const passwordInput = document.createElement("input");
+  label.setAttribute("id", "passwordInput");
   formContainer.appendChild(passwordInput);
 
-  const errorMsg = document.createElement('div');
-  errorMsg.style.color = 'red';
+  const errorMsg = document.createElement("div");
+  errorMsg.style.color = "red";
   formContainer.appendChild(errorMsg);
 
   content.appendChild(formContainer);
 
-  const actions = document.createElement('div');
+  const actions = document.createElement("div");
 
-  const continueButton = document.createElement('button');
-  continueButton.innerText = 'Continuar';
+  const continueButton = document.createElement("button");
+  continueButton.innerText = "Continuar";
   actions.appendChild(continueButton);
 
-  continueButton.addEventListener('click', () => {
+  continueButton.addEventListener("click", () => {
     let password = passwordInput.value;
     if (account.checkPassword(password)) {
       showAccountData(account);
     } else {
-      errorMsg.innerHTML = 'La contraseña ingresada es incorrecta';
+      errorMsg.innerHTML = "La contraseña ingresada es incorrecta";
     }
   });
 
-  const exitButton = document.createElement('button');
-  exitButton.innerText = 'Regresar';
+  const exitButton = document.createElement("button");
+  exitButton.innerText = "Regresar";
   actions.appendChild(exitButton);
 
-  exitButton.addEventListener('click', () => {
+  exitButton.addEventListener("click", () => {
     showAccounts();
   });
 
@@ -236,25 +276,33 @@ function showValidationForm(account) {
 }
 
 function showAccounts() {
-  content.innerHTML = '';
+  content.innerHTML = "";
 
-  const greeting = document.createElement('h1');
-  greeting.innerHTML = 'Bienvenido, seleccione una cuenta';
+  const greeting = document.createElement("h1");
+  greeting.innerHTML = "Bienvenido, seleccione una cuenta";
+  greeting.style.textAlign = "center";
   content.appendChild(greeting);
 
-  accounts.forEach(account => {
-    const div = document.createElement('div');
+  const accountContainer = document.createElement("div");
+  accountContainer.classList.add("accountContainer");
 
-    const name = document.createElement('h2');
+  accounts.forEach((account) => {
+    const div = document.createElement("div");
+    div.classList.add("account");
+
+    const name = document.createElement("h2");
     name.innerHTML = account.getFullName();
 
     div.appendChild(name);
-    content.appendChild(div);
+    accountContainer.appendChild(div);
 
-    div.addEventListener('click', () => {
+    div.addEventListener("click", () => {
       showValidationForm(account);
     });
   });
+
+  content.appendChild(accountContainer);
 }
 
+createHeader();
 showAccounts();
